80d42bfd47a1b23c54c0d357171afce4c52b62c9
diosurandito <diosurandito@gmail.com>
2021-08-18 16:24:52 +0700

    change: hashtag_spec adding test on get trending hashtag

--- spec/models/hashtag_spec.rb
+++ spec/models/hashtag_spec.rb
@@ -46,5 +46,44 @@ RSpec.describe 'hashtag model' do
             expect(stored_hashtag[1]).to be_nil
         end
     end
+
+    describe '#get_trending' do
+        it 'should return 5 top trending hashtags' do
+            db_client.query("insert into users (username, email) values ('dio', 'dio@mail.com')")
+            user_id = db_client.last_id
+            db_client.query("insert into posts (content, user_id) values ('this is post', #{user_id})")
+            post_id = db_client.last_id
+            db_client.query("insert into comments (content, post_id, user_id) values ('this is comment', #{post_id}, #{user_id})")
+            comment_id = db_client.last_id
+
+            db_client.query("insert into hashtags (tag, amount) values ('generasi', 1) on duplicate key update amount = values(amount) + 1")
+            tag_1 = db_client.last_id
+
+            db_client.query("insert into hashtags (tag, amount) values ('gigih', 1) on duplicate key update amount = values(amount) + 1")
+            tag_2 = db_client.last_id
+
+            i = 0
+            until i > 5 do
+                db_client.query("insert into hashtags (tag, amount) values ('generasi', 1) on duplicate key update amount = values(amount) + 1")
+                db_client.query("insert into post_hashtags (post_id, hashtag_id) values (#{post_id}, #{tag_1})")
+                i += 1
+            end
+
+            i = 0
+            until i > 3 do
+                db_client.query("insert into hashtags (tag, amount) values ('gigih', 1) on duplicate key update amount = values(amount) + 1")
+                db_client.query("insert into comment_hashtags (comment_id, hashtag_id) values (#{comment_id}, #{tag_2})")
+                i += 1
+            end
+
+            trending_tag = Hashtag.get_trending(db_client)
+
+            expect(trending_tag[0]['tag']).to eq('generasi')
+            expect(trending_tag[0]['amount']).to eq(6)
+
+            expect(trending_tag[1]['tag']).to eq('gigih')
+            expect(trending_tag[1]['amount']).to eq(4)
+        end
+    end
     
 end
